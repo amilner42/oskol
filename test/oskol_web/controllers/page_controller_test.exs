828ee7d810952b939f1204de372451f7c267268e
Arie Milner <amilner42@gmail.com>
2025-11-08 18:35:28 -0800

    mix phx.new . --no-ecto --no-mailer

---
+++ test/oskol_web/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule OskolWeb.PageControllerTest do
+  use OskolWeb.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get(conn, ~p"/")
+    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+  end
+end
