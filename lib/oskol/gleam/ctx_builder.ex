ebc7dd83b46713c738ecff0c9569f6b02b9a3587
Arie Milner <amilner42@gmail.com>
2026-09-22 10:46:08 -0700

    Format the merged ctx builder

--- lib/oskol/gleam/ctx_builder.ex
+++ lib/oskol/gleam/ctx_builder.ex
@@ -26,8 +26,7 @@ defmodule Oskol.Gleam.CtxBuilder do
   def build(opts \\ []) do
     {:ctx, Caps.Analysis.build(), Caps.Auth.build(), Caps.Copy.build(), Caps.Guests.build(),
      Caps.Ids.build(opts), Caps.Persistence.build(), Caps.Practice.build(), Caps.Puzzles.build(),
-     Caps.Records.build(),
-     Caps.Rooms.build(opts)}
+     Caps.Records.build(), Caps.Rooms.build(opts)}
   end
 
   @doc """
